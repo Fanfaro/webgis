7e0d7f3582b089f8c1b6c703bf6321db55c08c2b
Fanfaro <59604754+Fanfaro@users.noreply.github.com>
2020-01-16 17:27:37 +0100

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,14 @@
+
+require(["esri/Map", "esri/views/MapView"], function(Map, MapView, Expand){
+    let map1 = new Map({basemap:'topo'}); //słowo klucz new + nazwa modułu
+    let mapContainer = new MapView({
+        container: "itemMap", //miejsce gdzie chcemy osadzić mapę (klasa z pliku html)
+        map: map1, //nasza zmienna z mapą bazową zdefiniowaną wcześniej
+        zoom: 10,
+        center: [22.34, 51.15]
+    })
+    let basemapExpand = new Expand({
+        
+    })
+
+})
